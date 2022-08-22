a6877f40fff2a2b9841b068ab39518f72d8c5105
Tairraos <xiaole.tao@gmail.com>
2022-08-22 09:29:54 +0800

    refactor

---
+++ const.js
@@ -0,0 +1,7 @@
+const STEP_PARSING = "Parsing...";
+const STEP_WAITING = "Waiting...";
+const STEP_DOWNLOADING = "Downloading...";
+const STEP_DOWNLOADED = "Downloaded";
+const STEP_FAILED = "Failed";
+const STAT_OK = "ok";
+const STAT_ERROR = "error";
--- domutil.js
+++ domutil.js
@@ -1,3 +1,8 @@
+/**
+ * Generate new dom or selector exist dom like a simplest Sizzle.
+ * @param {string} arg 
+ * @returns 
+ */
 function $(arg) {
     if (arg.match(/^</)) {
         const template = document.createElement("template");
@@ -7,6 +12,12 @@ function $(arg) {
     return document.querySelector(arg);
 }
 
+/**
+ * Generate the top bar icon with text.
+ * @param {string} iconName 
+ * @param {string} textKey 
+ * @returns 
+ */
 function genIconTextButton(iconName, textKey) {
     const domStr = [
         `<button class="icon-text-btn btn-${iconName}" title="${i18n.get(textKey)}" data-i18n="title%${textKey}">`,
@@ -17,6 +28,12 @@ function genIconTextButton(iconName, textKey) {
     return $(domStr);
 }
 
+/**
+ * Generate the top bar icon with hover title.
+ * @param {string} iconName 
+ * @param {string} textKey 
+ * @returns 
+ */
 function genIconButton(iconName, textKey) {
     const domStr = [
         `<button class="icon-btn btn-${iconName}" title="${i18n.get(textKey)}" data-i18n="title%${textKey}">`,
@@ -26,18 +43,31 @@ function genIconButton(iconName, textKey) {
     return $(domStr);
 }
 
-function genIconDataStat(iconName, textKey, data) {
+/**
+ * Generate the footer stat.
+ * @param {string} iconName 
+ * @param {string} textKey 
+ * @param {string} counter 
+ * @returns 
+ */
+function genIconDataStat(iconName, textKey, counter) {
     const domStr = [
         `<div class="icon-data-stat stat-${iconName}" title="${i18n.get(textKey)}" data-i18n="title%${textKey}">`,
         `<svg class="icon ${iconName}"><use xlink:href="#icon-${iconName}"/></svg>`,
-        `<span class="data">${data}</span>`,
+        `<span class="data">${counter}</span>`,
         `</button>`
     ].join("");
     return $(domStr);
 }
+
 function genFolderTextBtn() {
     return $(`<button class="btn-stat" title="${i18n.get("Open folder")}" data-i18n="title%Open folder">${config.target}</button>`);
 }
+
+/**
+ * Generate the language selector.
+ * @returns
+ */
 function genLangSelector() {
     const domArr = [`<select class="select-lang" title="${i18n.get("Change language")}" data-i18n="title%Change language">`];
     i18n.langList.forEach((item) => {
@@ -47,6 +77,11 @@ function genLangSelector() {
     return $(domArr.join(""));
 }
 
+/**
+ * Generate the task box content.
+ * @param {object} task 
+ * @returns 
+ */
 function genTaskBox(task) {
     const domStr = [
         `<div class="task-box task-${task.domId}">`,
@@ -64,28 +99,49 @@ function genTaskBox(task) {
     return $(domStr);
 }
 
+/**
+ * Show a flash frame to Paste&Download button.
+ * @param {string} type 
+ */
 function flashPasteBtnUI(type) {
     const extClass = `border-flash-${type}`;
     dom.btnPaste.classList.add(extClass);
     setTimeout(() => dom.btnPaste.classList.remove(extClass), 1000);
 }
 
+/**
+ * Update the task content with the new data.
+ * @param {string} domId 
+ * @param {object} data 
+ */
 function updateTaskBoxUI(domId, data) {
-    const container = `.task-${domId}`;
-    data.cover && ($(`${container} .task-cover`).innerHTML = `<img src="${data.cover}" />`);
-    data.url && ($(`${container} .task-url`).innerText = data.url);
-    data.title && ($(`${container} .task-title`).innerText = i18n.get(data.title));
+    const base = `.task-${domId}`,
+        $container = $(base),
+        $title = $(base + " .task-title"),
+        $cover = $(base + " .task-cover"),
+        $url = $(base + " .task-url"),
+        $size = $(base + " .task-size"),
+        $status = $(base + " .task-status");
+    data.cover && ($cover.innerHTML = `<img src="${data.cover}" />`);
+    data.url && ($url.innerText = data.url);
+    data.title && ($title.innerText = i18n.get(data.title));
     if (data.size) {
-        $(`${container} .task-size`).innerText = (data.size / 1024).toFixed(1).replace(/\B(?=(?:\d{3})+\b)/g, ",") + "KB";
+        $size.innerText = (data.size / 1024).toFixed(1).replace(/\B(?=(?:\d{3})+\b)/g, ",") + "KB";
     }
     if (data.status) {
-        $(`${container}`).className = `task-box task-${domId} ${data.status.toLowerCase().replace(/\.+$/, "")}`;
-        $(`${container} .task-status`).innerText = i18n.get(data.status);
-        $(`${container} .task-status`).setAttribute("data-i18n", `innerText%${data.status}`);
+        $container.className = `task-box task-${domId} ${data.status.toLowerCase().replace(/\.+$/, "")}`;
+        $status.innerText = i18n.get(data.status);
+        $status.setAttribute("data-i18n", `innerText%${data.status}`);
+        if (data.status === STEP_DOWNLOADED) {
+            $container.classList.add("canopen");
+            $title.addEventListener("click", () => {
+                utils.openFolder(data.openpath);
+            });
+        }
     }
     if (data.progress) {
-        $(`${container} .task-progressbar`).classList.remove("hide");
-        $(`${container} .task-progress`).style.width = `${+data.progress * 2}px`;
+        $(`${base} .task-progressbar`).classList.remove("hide");
+        $(`${base} .task-progress`).style.width = `${+data.progress * 2}px`;
     }
     updateFooterStatUI();
 }
@@ -98,6 +154,10 @@ function updateFooterStatUI() {
     dom.dataFailed.innerText = counter.Failed || 0;
 }
 
+/**
+ * Update the download folder
+ * @param {string} folder 
+ */
 function updateFolderTextUI(folder) {
     if (folder !== "") {
         dom.btnFolderText.innerText = folder;
@@ -112,6 +172,10 @@ function updateFolderTextUI(folder) {
     }
 }
 
+/**
+ * Iterate throughout the UI, replacing all i18n strings.
+ * @param {string} lang 
+ */
 function updateI18nStringUI(lang) {
     const domList = document.querySelectorAll("[data-i18n]");
     config.lang = lang;
@@ -125,8 +189,12 @@ function updateI18nStringUI(lang) {
     printFooterLog("You have changed the display language.");
 }
 
-function printFooterLog(i18nKey) {
-    dom.staLogText.innerText = i18n.get(i18nKey);
+/**
+ * Show message in footer.
+ * @param {string} logKey
+ */
+function printFooterLog(logKey) {
+    dom.staLogText.innerText = i18n.get(logKey);
     clearTimeout(dom.staLogText.timer);
     dom.staLogText.timer = setTimeout(() => {
         dom.staLogText.innerText = "";
