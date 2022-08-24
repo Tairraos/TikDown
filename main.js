317872aaa64e83b817919f947fd04b2b4787650f
Tairraos <xiaole.tao@gmail.com>
2022-08-24 23:09:02 +0800

    released 1.2.1

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
     });
 
     // open debug
-    config.mainWindow.webContents.openDevTools();
+    // config.mainWindow.webContents.openDevTools();
 
     config.mainWindow.loadFile("index.html");
 }
